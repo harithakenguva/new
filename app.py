eb9357baf4d4a1692004209de9d21fdaa7d1e77b
haritha <haritha1@gmail.com>
2024-11-06 15:53:38 +0530

    modified app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import boto3
+import boto30
 client = boto3.client('ec2')
 response = client.run_instances(
    ImageId='ami-04b6019d38ea93034',
    InstanceType='t2.medium',
-   KeyName='git',
+   KeyName='giit',
    Maxcount=1,
    MinCount=1
 )
